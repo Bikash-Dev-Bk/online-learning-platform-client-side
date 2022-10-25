89a9c65e9debd92ff8c1fed1c0d3d9b87b537802
bikash-dev <bikash.dev.bk@gmail.com>
2022-10-25 14:20:21 +0600

    firebase tailwind daisyUI installation and folder stucture

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCpJ_x2iMGfuhcXb9FvC8pfnfoSNB8FejU",
+  authDomain: "learning-platform-client-a9126.firebaseapp.com",
+  projectId: "learning-platform-client-a9126",
+  storageBucket: "learning-platform-client-a9126.appspot.com",
+  messagingSenderId: "142597868411",
+  appId: "1:142597868411:web:d91d6a4d3c1ef7c0f40bde"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
