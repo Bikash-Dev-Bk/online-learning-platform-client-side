9cb50fcf4ae204142e6cbf61f5e673dc135ed7f9
bikash-dev <bikash.dev.bk@gmail.com>
2022-10-27 19:42:41 +0600

    FAQ page

--- src/Pages/FAQ/FAQ.js
+++ src/Pages/FAQ/FAQ.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './FQA.css';
 
 const FAQ = () => {
     return (
-        <div>
-            <h3 className='text-center'>This is FAQ</h3>
+        <div className='fqa-container'>
+            <h3 className='text-center text-3xl py-5 font-bolder fqa'>This is Frequently Asked Questions(FAQ)</h3>
         </div>
     );
 };
