6f3cffcf03e1c8ae8b8278b68d98bb981362c32e
bikash-dev <bikash.dev.bk@gmail.com>
2022-10-28 17:24:30 +0600

    toggle added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -2,8 +2,6 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../contexts/AuthProvider/AuthProvider";
 import { FaUser } from "react-icons/fa";
-import "./Header.css";
-// import {logo} from '../../../assets/images/logo.png';
 
 const Header = () => {
   const { user, logOut } = useContext(AuthContext);
@@ -54,6 +52,7 @@ const Header = () => {
               <li>
                 <Link to="/blog">Blog</Link>
               </li>
+              
 
               {user?.uid ? (
                 <>
@@ -111,19 +110,11 @@ const Header = () => {
               <Link to="/blog">Blog</Link>
             </li>
             <li>
-
-              <div class="flex items-center justify-center w-full">
-                <label for="toggleB" class="flex items-center cursor-pointer">
-                  <div class="relative">
-                    <input type="checkbox" id="toggleB" class="sr-only" />
-                    <div class="block bg-gray-600 w-14 h-8 rounded-full"></div>
-                    <div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
-                    <span class="absolute font-medium text-2xs uppercase right-1 text-white">Dark</span>
-                    <span class="absolute font-medium text-2xs uppercase right-8 text-white">Light </span>
-                  </div>
-                </label>
-              </div>
-
+              <label for="Toggle4" className="inline-flex items-center p-1 cursor-pointer dark:bg-gray-300 dark:text-gray-800">
+                <input id="Toggle4" type="checkbox" className="hidden peer" />
+                <span className="px-2 py-2 dark:bg-gray-600 peer-checked:dark:bg-gray-300">Dark</span>
+                <span className="px-2 py-2 dark:bg-gray-300 peer-checked:dark:bg-violet-400"> Light</span>
+              </label>
             </li>
           </ul>
         </div>
